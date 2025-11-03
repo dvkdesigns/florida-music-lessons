d357b46ddd9c2169dfb238000d320c6954ab7bdf
dvkdesigns <dvkdesigns@gmail.com>
2025-11-03 14:44:53 -0500

    Adding Header Addres Update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -114,7 +114,7 @@ export default function Header() {
               rel="noopener noreferrer"
               className="text-gray-700 hover:text-blue-600 transition-colors"
             >
-              7351 Wiles Rd, Coral Springs, FL
+              7837 W Sample Rd, Coral Springs, FL
             </a>
           </p>
 
